92daf47415556dcff7c21ac35fae8387b78777d4
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-09-26 16:27:14 -0400

    ready

--- src/item.js
+++ src/item.js
@@ -1,5 +1,4 @@
 class Item {
-
   constructor(item) {
     Object.keys(item).forEach((k) => {
       return this[k] = item[k]
@@ -8,34 +7,37 @@ class Item {
 
   render() {
     return (
-      `
-      <div data-id=${this.id} data-item-title='${this.name}' class='item-container'>
 
+      `<div data-id=${this.id} data-item-title='${this.name}' class='item-container'>
 
-          <div class='image-wrapper'>
-
-            <a href='${this.url}' target='_blank'><image src='${this.thumbnail[0].url}' class="thumbnail"/></a>
-
-          </div>
-
-          <div class='item-title-wrapper'>
-            <a href='${this.url}' target='_blank'>${this.name}</a>
-          </div>
+        <div class='image-wrapper'>
 
           <a href='${this.url}' target='_blank'>
-          <div class='item-bottom-branding'>
-            <span class="category">${this.categories ? (
-               `${this.categories[0]} | `
-             )
-               : (
-                 ""
-               )
-             }</span>
-               ${this.branding}
-          </div>
+            <image src='${this.thumbnail[0].url}' class="thumbnail"/>
           </a>
 
+        </div>
 
+        <div class='item-title-wrapper'>
+          <a href='${this.url}' target='_blank'>
+            ${this.name}
+          </a>
+        </div>
+
+        <a href='${this.url}' target='_blank'>
+          <div class='item-bottom-branding'>
+            <span class="category">
+              ${this.categories ? (
+                `${this.categories[0]} | `
+              )
+              : (
+                ""
+                )
+              }
+            </span>
+            ${this.branding}
+          </div>
+        </a>
       </div>`
     )
   }
